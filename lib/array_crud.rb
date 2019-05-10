7c2bfa1f4d5ff4491294adac5a319a0558da8f92
Ben Buchmann <ben_buchy92@hotmail.com>
2019-05-10 17:36:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,24 +3,21 @@ def European_finalists =
   
 end
 
-def European_finalists
+def European_finalists =
   ["Arsenal","Tottenham","liverpool","Chelsea"]
   end
 
 def European_finalists = << ["Ajax"]
   end
 
-def European_finalists.Unshift ["Barcelona"]
-  
-end
+def European_finalists.Unshift = ["Barcelona"]
+  end
 
-def remove_element_from_end_of_array(array)
-  
-end
+def European_finalists.Pop = ["Tottenham"]
+  end
 
-def remove_element_from_start_of_array(array)
-  
-end
+def European_finalists.Shift = ["Barcelona"]
+  end
 
 def retrieve_element_from_index(array, index_number)
   
