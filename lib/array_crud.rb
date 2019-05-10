55712c86bbe38d26caceb07ea80c265032f466f1
Ben Buchmann <ben_buchy92@hotmail.com>
2019-05-10 17:46:09 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,32 @@
-def create_an_empty_array
+def European_finalists =
+  ["Arsenal","Tottenham","Liverpool","Chelsea"]
   
 end
 
-def create_an_array
+def European_finalists =
+  ["Arsenal","Tottenham","liverpool","Chelsea"]
+  end
+
+def European_finalists = << ["Ajax"]
+  end
+
+def European_finalists.Unshift = ["Barcelona"]
+  end
+
+def European_finalists.Pop = ["Tottenham"]
+  end
+
+def European_finalists.Shift = ["Barcelona"]
+  end
+
+def European_finalists.Index = ["Chelsea"]
   
 end
 
-def add_element_to_end_of_array(array, element)
+def European_finalists = ["Arsenal"]
   
 end
 
-def add_element_to_start_of_array(array, element)
-  
-end
-
-def remove_element_from_end_of_array(array)
-  
-end
-
-def remove_element_from_start_of_array(array)
-  
-end
-
-def retrieve_element_from_index(array, index_number)
-  
-end
-
-def retrieve_first_element_from_array(array)
-  
-end
-
-def retrieve_last_element_from_array(array)
+def European_finalists = ["liverpool"]
   
 end
