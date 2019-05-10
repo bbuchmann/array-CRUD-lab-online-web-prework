f3d7a75b959d544ba33b0ec7c6c14051354fdc78
Ben Buchmann <ben_buchy92@hotmail.com>
2019-05-10 17:31:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,4 +1,4 @@
-def European_finalists 
+def European_finalists =
   ["Arsenal","Tottenham","Liverpool","Chelsea"]
   
 end
@@ -7,7 +7,7 @@ def European_finalists
   ["Arsenal","Tottenham","liverpool","Chelsea"]
   end
 
-def European_finalists
+def European_finalists = 
   
   
 end
